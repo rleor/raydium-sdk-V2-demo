bfbc617af30ea6961eee00628e5890baed5188ee
rayna.sats <rayna@sating.io>
2024-11-12 11:11:37 +0800

    temp

---
+++ src/monitor/binance.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+const BINANCE_NEWS_URL = "https://www.binance.com/bapi/composite/v1/public/cms/article/list/query?type=1&catalogId=48&pageNo=1&pageSize=20";
+const interval = 60 // seconds
+
+interface NewsResp {
+    code: string
+    data: NewsData
+}
+interface NewsData {
+    catalogs: Array<Catalog>
+}
+interface Catalog {
+    catalogId: number
+    articles: Array<Article>
+}
+interface Article {
+    id: number
+    title: string
+}
+
+const start = async () => {
+    const resp = await axios.get<NewsResp>(BINANCE_NEWS_URL);
+    console.log(resp);
+}
+
+start();
